69fe9cde95f2f14bd4af07190f5f3fa2af651e65
B3T4C0R3 <B3T4C0R3@users.noreply.github.com>
2020-10-05 21:25:32 +0000

    deploy: B3T4C0R3/betacore@946d081422b5cd3b9f2869038ba3bda60bc763d1

---
+++ text/brand.js
@@ -0,0 +1,16 @@
+const brand = {
+  betacore: {
+    name: 'BetaCore',
+    desc: 'BetaCore - Websolutions',
+    prefix: 'derk',
+    footerText: 'BetaCore',
+    logoText: 'BetaCore',
+    projectName: 'BetaCore',
+    url: 'betacore.nl',
+    img: '/static/images/betacore-logo.png',
+    notifMsg:
+      'Donec sit amet nulla sed arcu pulvinar ultricies commodo id ligula.'
+  }
+}
+
+export default brand
---
+++ text/link.js
@@ -0,0 +1,10 @@
+const link = {
+  betacore: {
+    home: '/',
+    contact: '/contact',
+    login: '/login',
+    register: '/register'
+  }
+}
+
+export default link
